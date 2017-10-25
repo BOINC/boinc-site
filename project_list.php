0cf04e0ef230b7fb885c79669b16205dbeb15510
David Anderson <davea@ssl.berkeley.edu>
2017-10-25 14:30:32 -0700

    Assign unique integer IDs to projects and account managers

--- project_list.php
+++ project_list.php
@@ -8,7 +8,6 @@ $dir = getcwd();
 chdir("/mydisks/a/users/boincadm/projects/dev/html/user");
 require_once("../inc/translation.inc");
 require_once("projects.inc");
-require_once("account_managers.inc");
 require_once("get_platforms.inc");
 chdir($dir);
 
@@ -35,6 +34,7 @@ foreach ($areas as $area) {
         if (strlen($p->summary)) {
             $np->summary = $p->summary;
         }
+        $np->id = $p->id;
         $np->home = $p->home;
         $np->general_area = $area_name;
         $np->specific_area = $p->area;
@@ -48,6 +48,7 @@ foreach ($areas as $area) {
 foreach($proj_list as $p) {
     echo "    <project>
         <name>$p->name</name>
+        <id>$p->id</id>
         <url>$p->url</url>
         <general_area>$p->general_area</general_area>
         <specific_area>$p->specific_area</specific_area>
@@ -76,15 +77,12 @@ foreach($proj_list as $p) {
 }
 
 foreach ($account_managers as $am) {
-    $name = $am->name;
-    $url = $am->url;
-    $desc = $am->description;
-    $image = $am->logo;
     echo "   <account_manager>
-        <name>$name</name>
-        <url>$url</url>
-        <description>$desc</description>
-        <image>https://boinc.berkeley.edu/images/$image</image>
+        <name>$am->name</name>
+        <id>$am->id</id>
+        <url>$am->url</url>
+        <description>$am->description</description>
+        <image>https://boinc.berkeley.edu/images/$am->logo</image>
     </account_manager>
 ";
 }
