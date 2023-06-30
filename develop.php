1d88cfccd197228f3e463cc2b1afab636cdb8fb2
Anand Bhat <7332442+anand-bhat@users.noreply.github.com>
2023-06-30 23:30:04 +1000

    Update develop.php

--- develop.php
+++ develop.php
@@ -10,19 +10,19 @@ it's distributed under the LGPL open source license.
 The BOINC source code is stored in
 <a href=https://github.com/BOINC/boinc>a GitHub repository</a>.
 If you're not familiar with Git,
-<a href=https://boinc.berkeley.edu/trac/wiki/SourceCodeGit>read this</a>.
+<a href=https://github.com/BOINC/boinc/wiki/SourceCodeGit>read this</a>.
 <p>
-Here are <a href=https://boinc.berkeley.edu/trac/wiki/SoftwareBuilding>instructions for building BOINC</a> on various platforms.
+Here are <a href=https://github.com/BOINC/boinc/wiki/SoftwareBuilding>instructions for building BOINC</a> on various platforms.
 <p>
 BOINC is maintained and developed partly
 by the UC Berkeley BOINC project,
 and partly by volunteers.
 If you're interested in participating, visit the GitHub repo.
 Most communication takes place there.
-Also subscribe to the <a href=https://boinc.berkeley.edu/trac/wiki/EmailLists>boinc_dev</a> email list.
+Also subscribe to the <a href=https://github.com/BOINC/boinc/wiki/EmailLists>boinc_dev</a> email list.
 <p>
 Keeping BOINC working also involves a number of
-<a href=https://boinc.berkeley.edu/trac/wiki/AdminTasks>maintenance tasks</a>
+<a href=https://github.com/BOINC/boinc/wiki/AdminTasks>maintenance tasks</a>
 such as release management, managing the translation system, and so on.
 
 <h3>APIs</h2>
@@ -31,19 +31,19 @@ BOINC is an 'open system' with lots of interacting pieces:
 client, GUI, server, account managers, statistics websites,
 remote job submission systems, etc.
 These components communicate through
-<a href=https://boinc.berkeley.edu/trac/wiki/SoftwareAddon>a set of RPC interfaces</a>.
+<a href=https://github.com/BOINC/boinc/wiki/SoftwareAddon>a set of RPC interfaces</a>.
 
 <h3>Events</h2>
 <p>
-In the past we've had <a href=https://boinc.berkeley.edu/trac/wiki/BoincEvents>a yearly series of 'workshops'</a>
+In the past we've had <a href=https://github.com/BOINC/boinc/wiki/BoincEvents>a yearly series of 'workshops'</a>
 where everyone involved in BOINC
 (developers, projects, volunteers) meets and talks.
 
 <h3>Miscellaneous</h2>
 <p>
 <ul>
-<li> <a href=https://boinc.berkeley.edu/trac/wiki/CodingStyle>Coding style</a>
-<li> <a href=https://boinc.berkeley.edu/trac/wiki/SoftwareDevelopment>Design documents</a>
+<li> <a href=https://github.com/BOINC/boinc/wiki/CodingStyle>Coding style</a>
+<li> <a href=https://github.com/BOINC/boinc/wiki/SoftwareDevelopment>Design documents</a>
 </ul>
 ";
 text_end();
